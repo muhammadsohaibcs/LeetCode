61ffd88f727649a1e6519dd1facb28891dad0a4a
muhammadsohaibcs <muhammadsohaibcs@gmail.com>
2025-11-21 00:56:43 +0500

    Time: 182 ms (5.04%) | Memory: 27.4 MB (8.74%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,16 +4,14 @@ class Solution:
             return 0
         l =[]
         l.append(float("inf"))
+        profit = float("-inf")
         for i in range(1, len(prices)):
             if l[-1] < prices[i-1]:
                 l.append (l[-1])
             else:
                 l.append (prices [i-1])
-        profit = float("-inf")
-        for i in range(1, len(prices)):
             profit =max(profit , prices[i]- l[i])
         return profit
-
 
 
         
