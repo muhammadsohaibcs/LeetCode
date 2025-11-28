1ad1f95857cd778b62081657c6bee6cf78990b02
muhammadsohaibcs <muhammadsohaibcs@gmail.com>
2025-11-28 16:26:28 +0500

    Time: 55 ms (15.46%) | Memory: 19.8 MB (57.49%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -8,7 +8,7 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         temp1 = head
         temp2 = head
-        while temp1 and temp2 and temp2.next:
+        while temp2 and temp2.next:
             temp1 = temp1.next
             temp2 = temp2.next.next
             if temp1 == temp2:
